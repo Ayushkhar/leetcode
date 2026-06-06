c27f6c5d8ced1c9262017ee716290f9b1ac24ae9
Suryansh khare <99049123+Ayushkhar@users.noreply.github.com>
2026-06-06 22:26:29 +0530

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.py
@@ -0,0 +1,12 @@
+# Last updated: 6/6/2026, 10:26:29 PM
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        a = s.split()
+        b = []
+
+
+        for i in range(len(a) - 1, -1, -1):
+            b.append(a[i])
+
+        return ' '.join(b)
+
