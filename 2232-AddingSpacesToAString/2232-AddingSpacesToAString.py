8544085295e55dbda41c100abe96c32ee5bacdca
Suryansh khare <99049123+Ayushkhar@users.noreply.github.com>
2026-06-06 22:24:33 +0530

    Create 2232-AddingSpacesToAString

---
+++ 2232-AddingSpacesToAString/2232-AddingSpacesToAString.py
@@ -0,0 +1,21 @@
+# Last updated: 6/6/2026, 10:24:34 PM
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
+        result = [''] * (len(s) + len(spaces))
+        write_pos = 0
+        read_pos = 0
+        
+        for space_pos in spaces:
+            while read_pos < space_pos:
+                result[write_pos] = s[read_pos]
+                write_pos += 1
+                read_pos += 1
+            result[write_pos] = ' '
+            write_pos += 1
+            
+        while read_pos < len(s):
+            result[write_pos] = s[read_pos]
+            write_pos += 1
+            read_pos += 1
+            
+        return ''.join(result)
