2b8026261ee143351216f4675c4a5006022851f9
Suryansh khare <99049123+Ayushkhar@users.noreply.github.com>
2026-06-06 22:25:41 +0530

    Create 415-AddStrings

---
+++ 415-AddStrings/415-AddStrings.py
@@ -0,0 +1,10 @@
+# Last updated: 6/6/2026, 10:25:41 PM
+import sys
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        sys.set_int_max_str_digits(10000)
+        a=int(num1)
+        b=int(num2)
+        res=a+b
+        return str(res)
+        
