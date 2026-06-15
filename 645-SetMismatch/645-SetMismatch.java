4c5e01530dc09ab6ea7735ef12007c8c33d4c13a
Suryansh khare <99049123+Ayushkhar@users.noreply.github.com>
2026-06-15 22:28:29 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.java
@@ -0,0 +1,30 @@
+// Last updated: 6/15/2026, 10:28:29 PM
+class Solution {
+    public int[] findErrorNums(int[] nums) 
+    {
+        int i=0;
+        while(i<nums.length)
+        {
+            int correct =nums[i]-1;
+            if(nums[i]!=nums[correct])
+            {
+                int temp =nums[i];
+                nums[i]=nums[correct];
+                nums[correct]= temp;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        for(int j = 0; j <nums.length; j++)
+        {
+            if(nums[j]-1!=j || (j+1)!=nums[j])
+            {
+                return new int[]{nums[j],j+1};
+            } 
+        }
+        return new int[]{-1,-1};
+          
+    }
+}
